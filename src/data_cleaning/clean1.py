e1b01f1ea6f49d2536c718a1f75f1a14a19d78bd
DavidZhan23 <qiyuan.zhan23@imperial.ac.uk>
2024-05-14 11:44:50 +0100

    preprocessing

--- src/data_cleaning/clean1.py
+++ src/data_cleaning/clean1.py
@@ -2,12 +2,13 @@
 import pandas as pd
 import numpy as np
 import os
+
 # %%
 users_df = pd.read_csv('../../dataset/users.csv')
 users_df = users_df.sort_values(by=['id'])
 survey_df = pd.read_csv('../../dataset/post-survey-responses.csv')
 survey_df = survey_df.sort_values(by=['user_id'])
-print(users_df.head())
+
 # %%
 # create a new dataframe with columns user_id, 1, 4, 5, 6, 7, personality
 df = pd.DataFrame(columns=['user_id','Efficiency','Enjoyment','Boredom','Confidence','Anxiety','Personality','group_type'])
@@ -15,19 +16,31 @@ df = pd.DataFrame(columns=['user_id','Efficiency','Enjoyment','Boredom','Confide
 i = 0
 for user_id in survey_df['user_id']:
     df.loc[i, 'user_id'] = user_id
-    df.loc[i, 'Efficiency'] = survey_df.loc[(survey_df['user_id'] == user_id) & (survey_df['question_id'] == 1)]['description'].values[0]
-    df.loc[i, 'Enjoyment'] = survey_df.loc[(survey_df['user_id'] == user_id) & (survey_df['question_id'] == 4)]['description'].values[0]
-    df.loc[i, 'Boredom'] = survey_df.loc[(survey_df['user_id'] == user_id) & (survey_df['question_id'] == 5)]['description'].values[0]
-    df.loc[i, 'Confidence'] = survey_df.loc[(survey_df['user_id'] == user_id) & (survey_df['question_id'] == 6)]['description'].values[0]
-    df.loc[i, 'Anxiety'] = survey_df.loc[(survey_df['user_id'] == user_id) & (survey_df['question_id'] == 7)]['description'].values[0]
-    df.loc[i, 'Personality'] = users_df.loc[users_df['id'] == user_id]['personality'].values[0]
-    df.loc[i, 'group_type'] = users_df.loc[users_df['id'] == user_id]['group_type'].values[0]
+
+    efficiency = survey_df.loc[(survey_df['user_id'] == user_id) & (survey_df['question_id'] == 1)]['description'].values
+    enjoyment = survey_df.loc[(survey_df['user_id'] == user_id) & (survey_df['question_id'] == 4)]['description'].values
+    boredom = survey_df.loc[(survey_df['user_id'] == user_id) & (survey_df['question_id'] == 5)]['description'].values
+    confidence = survey_df.loc[(survey_df['user_id'] == user_id) & (survey_df['question_id'] == 6)]['description'].values
+    anxiety = survey_df.loc[(survey_df['user_id'] == user_id) & (survey_df['question_id'] == 7)]['description'].values
+    personality = users_df.loc[users_df['id'] == user_id]['personality'].values
+    group_type = users_df.loc[users_df['id'] == user_id]['group_type'].values
+
+    df.loc[i, 'Efficiency'] = efficiency[0] if len(efficiency) > 0 else np.nan
+    df.loc[i, 'Enjoyment'] = enjoyment[0] if len(enjoyment) > 0 else np.nan
+    df.loc[i, 'Boredom'] = boredom[0] if len(boredom) > 0 else np.nan
+    df.loc[i, 'Confidence'] = confidence[0] if len(confidence) > 0 else np.nan
+    df.loc[i, 'Anxiety'] = anxiety[0] if len(anxiety) > 0 else np.nan
+    df.loc[i, 'Personality'] = personality[0] if len(personality) > 0 else np.nan
+    df.loc[i, 'group_type'] = group_type[0] if len(group_type) > 0 else np.nan
+
     i += 1
+
 # %%
 # drop duplicates
 df = df.drop_duplicates()
 # drop rows with NaN values
 df = df.dropna()
+
 # %%
 # Create the cleaned_dataset folder if it doesn't exist
 os.makedirs('../../cleaned_dataset', exist_ok=True)
